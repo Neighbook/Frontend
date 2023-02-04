0c8ef7dbf4cf10c6d18eb61f054b2c8facf28f95
Nathan Kroichvili <nathankr@free.fr>
2023-02-04 16:57:47 +0100

    fix type

--- src/config/api_config.ts
+++ src/config/api_config.ts
@@ -1,6 +1,6 @@
 export const config = {
-	api_base_url: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000',
-	auth_route: "/auth",
-	user_route: "/user",
-	social_route: "/social"
+    api_base_url: import.meta.env.VITE_API_BASE_URL as string || 'http://localhost:3000',
+    auth_route: "/auth",
+    user_route: "/user",
+    social_route: "/social"
 };
