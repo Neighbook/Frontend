cf58e78834c4adf97214ec771e88102884059efd
Kroissan <66138267+Kroissan@users.noreply.github.com>
2023-02-08 10:32:04 +0100

    page mon compte + file upload (#13)
    * page mon compte + file upload
    
    * del pfp
    
    * renew file url logic
    
    ---------
    
    Co-authored-by: NathanKr <nathankr@orange.fr>

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,12 +3,13 @@ import Box from '@mui/material/Box';
 import {Skeleton} from "@mui/material";
 
 export const CompteLoading = () => (
-    <Box sx={{ width: '100%' }}>
-        <Skeleton variant="text" sx={{ fontSize: '1rem', width: "50%", mb: 5}} />
-        <Skeleton variant="circular" sx={{width: "20vh", height: "20vh", mb: 5}}/>
-        <Skeleton variant="rectangular" sx={{width: "80%", height: "5vh", mb: 3}} />
-        <Skeleton variant="rectangular" sx={{width: "80%", height: "5vh", mb: 3}} />
-        <Skeleton variant="rectangular" sx={{width: "80%", height: "5vh", mb: 3}} />
-        <Skeleton variant="rectangular" sx={{width: "80%", height: "5vh", mb: 3}} />
+    <Box sx={{ width: '100%' }} ml={5} mr={5}>
+        <Skeleton variant="text" sx={{ fontSize: '2rem', width: "200px", mb: 0.2}} />
+        <Skeleton variant="text" sx={{ fontSize: '1.3rem', width: "500px", mb: 5}} />
+        <Skeleton variant="circular" sx={{width: "20vh", height: "20vh", minHeight: "200px", minWidth: "200px", mb: 10, ml: 2, aspectRatio: "1/1"}}/>
+        <Skeleton variant="rectangular" sx={{width:"650px", height: "60px", mb: 3}} />
+        <Skeleton variant="rectangular" sx={{width:"650px", height: "60px", mb: 3}} />
+        <Skeleton variant="rectangular" sx={{width:"650px", height: "60px", mb: 3}} />
+        <Skeleton variant="rectangular" sx={{width:"650px", height: "60px", mb: 3}} />
     </Box>
 );
