5bfd86258e074787044210f5c972a2f9de25e503
Lilian BOUVIER <lilian.bouvier@etu.unistra.fr>
2023-02-12 22:47:41 +0100

    messagerie fonctionelle

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -3,13 +3,15 @@ import type { User } from "../hook/user";
 import React, { useState } from 'react';
 
 type Props = {
-	me: User;
-	receiver: User;
-  messages: [];
-  sendMessage: Function;
+    receiver: User;
+    messages: Array<{
+        sender: string;
+        message: string;
+    }>;
+    sendMessage: Function;
 }
 
-export default function ChatRoom({ me, receiver, messages, sendMessage }: Props) {
+export default function ChatRoom({ receiver, messages, sendMessage }: Props) {
     const [draft, setDraft] = useState<String>('');
 
     const send = () => {
@@ -32,12 +34,34 @@ export default function ChatRoom({ me, receiver, messages, sendMessage }: Props)
                 sx={{
                     display: 'flex',
                     flexDirection: 'column',
+                    justifyContent: 'flex-end',
                     alignItems: 'center',
                     gap: '5px',
-                    flexGrow: 1
+                    flexGrow: 1,
+                    width: '100%'
                 }}
             >
-
+                {
+                    messages.map( (msg, id) => (
+                        <Box key={id} sx={{
+                            width: '100%',
+                            display: 'flex',
+                            justifyContent: msg.sender === 'me' ? 'flex-end' : 'flex-start'
+                        }}>
+                            <Box sx={{
+                                background: msg.sender === 'me' ? '#E4E6BF' : '#879472',
+                                padding: '5px 10px',
+                                borderRadius: '5px',
+                                maxWidth: 'calc(100% - 50px)'
+                            }}>
+                                <Typography variant="h6" fontWeight='bold' color='#64675A' noWrap component="div">
+                                    {msg.message}
+                                </Typography>
+                            </Box>
+                            
+                        </Box>
+                    ))
+                }
             </Box>
 
             <Box sx={{ margin: '10px', width: '100%', display: 'flex', position: 'relative', alignItems: 'center', gap: '5px' }}>
@@ -50,7 +74,7 @@ export default function ChatRoom({ me, receiver, messages, sendMessage }: Props)
                     onKeyDown={e => {
                         const key=e.keyCode || e.which;
                         if (key===13){
-                            send()
+                            send();
                         }
                     }}
                 /> 
