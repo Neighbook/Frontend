c8506b65a6cf003433587099cfa9087af9960133
Ayoub <57446997+ayoubqrt@users.noreply.github.com>
2023-06-13 15:44:44 +0200

    Revert "Ajouts premiers fichiers messagerie (#17)"
    This reverts commit f6d8ae5ac4de09dea8d8857a53c95fe93fac0e43.

--- src/views/Messagerie.tsx
+++ src/views/Messagerie.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import { Typography } from '@mui/material';
-import ChatProfile from '../components/messagerie/ChatProfile';
-import PermanentDrawerRight from '../components/messagerie/PermanentDrawerRight';
 
 const Messagerie = () => (
-    <div>
-        <Typography variant="h1">
-            Messagerie
-        </Typography>
-        <ChatProfile profilePictureSrc='' name='Ayouuuuub'/>
-        <PermanentDrawerRight friends={[{
-            nom:'ayoub',
-            prenom:'ayoub'
-        }]}/>
-    </div>
+    <Typography variant="h1">
+        Messagerie
+    </Typography>
 );
 
 export default Messagerie;
